7e55d76fd1b42ad1c878738fcc8092ce17a9fdac
mohammad yusuf noor habibi <myusufnhabibi@gmail.com>
2023-06-30 23:22:55 +0700

    penambahan rekap dashboard admin

--- resources/views/layouts/admin/script.blade.php
+++
@@ -1,8 +0,0 @@
-  <!-- Libs JS -->
-  <script src="{{ asset('tabler') }}/dist/libs/apexcharts/dist/apexcharts.min.js?1684106062" defer></script>
-  <script src="{{ asset('tabler') }}/dist/libs/jsvectormap/dist/js/jsvectormap.min.js?1684106062" defer></script>
-  <script src="{{ asset('tabler') }}/dist/libs/jsvectormap/dist/maps/world.js?1684106062" defer></script>
-  <script src="{{ asset('tabler') }}/dist/libs/jsvectormap/dist/maps/world-merc.js?1684106062" defer></script>
-  <!-- Tabler Core -->
-  <script src="{{ asset('tabler') }}/dist/js/tabler.min.js?1684106062" defer></script>
-  <script src="{{ asset('tabler') }}/dist/js/demo.min.js?1684106062" defer></script>
